6d4fb8885656b877154304127cac9fdf6b12da2e
mdfahimislam <mdfahim1194@gmail.com>
2022-01-03 12:48:44 +0600

    InetAddress in java

--- src/socket/InetAddressExample.java
+++ src/socket/InetAddressExample.java
@@ -4,7 +4,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class InetAddressExample {
-    public static void main(String args[]){
+    public static void main(String[] args){
         try {
             InetAddress address = InetAddress.getLocalHost();
             System.out.println(address.getHostAddress());
